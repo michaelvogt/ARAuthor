8949a4ab960facb3955b00d1123ce0f48752f0b9
Michael Vogt <info@michaelvogt.eu>
2018-06-06 00:56:25 +0900

    Image overlay

--- app/src/main/java/eu/michaelvogt/ar/author/AuthorActivity.java
+++ app/src/main/java/eu/michaelvogt/ar/author/AuthorActivity.java
@@ -3,9 +3,6 @@ package eu.michaelvogt.ar.author;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-
-import java.util.List;
 
 import eu.michaelvogt.ar.author.data.AuthorViewModel;
 import eu.michaelvogt.ar.author.data.Marker;
@@ -21,33 +18,39 @@ public class AuthorActivity extends AppCompatActivity {
         viewModel = ViewModelProviders.of(this).get(AuthorViewModel.class);
         initViewModel();
 
-        setContentView(R.layout.activity_edit);
+        setContentView(R.layout.activity_author);
     }
 
     private void initViewModel() {
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_092548.jpg",
                 "kameishi_sign",
-                "城上神社"));
+                "城上神社",
+                1.365f));
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_100912.jpg",
                 "hidakaya_sign",
-                "日高屋"));
+                "日高屋",
+                0.644f));
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_110133.jpg",
                 "kouengate_sign",
-                "石見公園"));
+                "石見公園",
+                0.615f));
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_125435.jpg",
                 "opera_sign",
-                "大森座"));
+                "大森座",
+                0.671f));
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_101603.jpg",
                 "jiso",
-                "観世音寺"));
+                "観世音寺",
+                0));
         viewModel.addMarker(new Marker(
                 "/Touristar/Markers/IMG_20180419_100646.jpg",
                 "aoyamake_sign",
-                "青山家"));
+                "青山家",
+                0.648f));
     }
 }
