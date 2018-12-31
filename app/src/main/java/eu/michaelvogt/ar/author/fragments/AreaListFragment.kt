84c620435aa4801a5a088d6ec44364d7a12572f6
Michael Vogt <info@michaelvogt.eu>
2018-12-31 15:21:20 +0100

    Copyright update

--- app/src/main/java/eu/michaelvogt/ar/author/fragments/AreaListFragment.kt
+++ app/src/main/java/eu/michaelvogt/ar/author/fragments/AreaListFragment.kt
@@ -1,6 +1,6 @@
 /*
 ARTester - AR for tourists by tourists
-Copyright (C) 2018  Michael Vogt
+Copyright (C) 2018, 2019  Michael Vogt
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
